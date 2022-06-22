e95720991b665e2e6dde4cef57c0330c435f4fde
julianmedwards <julian.michael.edwards@gmail.com>
2022-06-22 14:13:51 -0500

    Exercise 10 complete.

--- scripts/10-volume-of-sphere.js
+++ scripts/10-volume-of-sphere.js
@@ -1,1 +1,13 @@
 'use strict'
+
+let form = document.forms.MyForm
+form.addEventListener('submit', function (event) {
+    event.preventDefault()
+    let radius = Number(this.radius.value)
+
+    let result = (4 / 3) * Math.PI * Math.pow(radius, 3)
+    result = result.toFixed(3)
+
+    this.volume.value = result
+    form.radius.focus()
+})
