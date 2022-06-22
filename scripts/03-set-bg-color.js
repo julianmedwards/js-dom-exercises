1ca9d821c3ccdbebf9bd799c4f5525b348b098bf
julianmedwards <julian.michael.edwards@gmail.com>
2022-06-22 10:51:40 -0500

    Exercise 3 complete.

--- scripts/03-set-bg-color.js
+++ scripts/03-set-bg-color.js
@@ -1,1 +1,9 @@
-"use strict";
+'use strict'
+
+function delayedBGColorChange() {
+    setTimeout(function () {
+        document.getElementById('two').style.backgroundColor = '#89CFF0'
+    }, 3500)
+}
+
+delayedBGColorChange()
