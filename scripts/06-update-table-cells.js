cd9b57e0e2749b44eb5729e84c4c46d6769acfcb
julianmedwards <julian.michael.edwards@gmail.com>
2022-06-22 11:45:00 -0500

    Exercise 6 complete.

--- scripts/06-update-table-cells.js
+++ scripts/06-update-table-cells.js
@@ -1,1 +1,22 @@
-"use strict";
+'use strict'
+
+function changeContent() {
+    let form = document.getElementById('table-form')
+    let row = form['row-num'].value
+    let col = form['column-num'].value
+    let newText = form['cell-text'].value
+
+    let tbody = document.getElementById('myTable').firstElementChild
+
+    let trow = tbody.children[row - 1]
+    if (trow) {
+        let tcol = trow.children[col - 1]
+        if (tcol) {
+            tcol.textContent = newText
+        } else {
+            alert('There is no such column!')
+        }
+    } else {
+        alert('There is no such row!')
+    }
+}
