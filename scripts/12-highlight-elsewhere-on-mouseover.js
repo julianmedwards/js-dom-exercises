159496b0969b953028d0002c24bf1d98ee9bbb4c
julianmedwards <julian.michael.edwards@gmail.com>
2022-06-22 14:57:17 -0500

    Exercise 12 complete

--- scripts/12-highlight-elsewhere-on-mouseover.js
+++ scripts/12-highlight-elsewhere-on-mouseover.js
@@ -1,1 +1,16 @@
 'use strict'
+
+let hoverArea = document.getElementById('hoverArea')
+let hoverEls = document.getElementsByClassName('hoverHighlight')
+
+hoverArea.addEventListener('mouseenter', function () {
+    for (let el of hoverEls) {
+        el.style.backgroundColor = 'yellow'
+    }
+})
+
+hoverArea.addEventListener('mouseout', function () {
+    for (let el of hoverEls) {
+        el.style.backgroundColor = ''
+    }
+})
