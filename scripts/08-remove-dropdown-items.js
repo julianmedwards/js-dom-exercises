54fc76c2efbfbf5fec1139dd783c64f4554208c4
julianmedwards <julian.michael.edwards@gmail.com>
2022-06-22 13:28:15 -0500

    Exercise 8 complete.

--- scripts/08-remove-dropdown-items.js
+++ scripts/08-remove-dropdown-items.js
@@ -1,1 +1,10 @@
 'use strict'
+
+function removecolor() {
+    let dropdown = document.getElementById('colorSelect')
+    if (dropdown.childElementCount > 0) {
+        dropdown.remove(dropdown.selectedIndex)
+    } else {
+        alert('No more options to delete!')
+    }
+}
