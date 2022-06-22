07cc5fd0b5e2c27efc84e1b3ec5f057085807d6e
julianmedwards <julian.michael.edwards@gmail.com>
2022-06-22 11:24:44 -0500

    Exercise 5 complete.

--- scripts/05-add-rows-to-tabl.js
+++ scripts/05-add-rows-to-tabl.js
@@ -1,1 +1,16 @@
-"use strict";
+'use strict'
+
+function insert_Row() {
+    let table = document.getElementById('sampleTable')
+    let tbody = table.firstElementChild
+    for (let i = 0; i < tbody.children.length; i++) {
+        console.log(tbody.children[i])
+        var newRowNum = i + 1
+    }
+    console.log(newRowNum)
+    let newRow = document.createElement('tr')
+    let newCell = document.createElement('td')
+    newCell.textContent = 'Row' + newRowNum + ' cell1'
+    newRow.append(newCell)
+    tbody.append(newRow)
+}
