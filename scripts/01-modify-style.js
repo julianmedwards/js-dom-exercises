8409218bbf6d3f804bee4d734c7db8f8c9f0587a
julianmedwards <julian.michael.edwards@gmail.com>
2022-06-22 10:13:36 -0500

    Exercise 1 complete.

--- scripts/01-modify-style.js
+++ scripts/01-modify-style.js
@@ -1,1 +1,8 @@
-"use strict";
+'use strict'
+
+function js_style() {
+    let p = document.getElementById('text')
+    p.style.fontFamily = 'monospace'
+    p.style.fontSize = '18px'
+    p.style.color = '#00008b'
+}
