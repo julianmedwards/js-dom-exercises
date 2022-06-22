1c111618d27f27976d28e5c2229c602977d5912a
julianmedwards <julian.michael.edwards@gmail.com>
2022-06-22 13:38:20 -0500

    Exercise 9 complete.

--- scripts/09-get-dropdown-items.js
+++ scripts/09-get-dropdown-items.js
@@ -1,1 +1,19 @@
 'use strict'
+
+function getOptions() {
+    let dropdown = document.getElementById('mySelect')
+    if (dropdown.childElementCount > 0) {
+        let optionArr = []
+        for (let option of dropdown.options) {
+            optionArr.push(option.value)
+        }
+        alert(
+            'Number of options: ' +
+                optionArr.length +
+                '\nOptions: ' +
+                optionArr.join(', ')
+        )
+    } else {
+        alert('There are no options!')
+    }
+}
