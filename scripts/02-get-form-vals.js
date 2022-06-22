64338a66ddf7deb186e3f5c737eafa052b366563
julianmedwards <julian.michael.edwards@gmail.com>
2022-06-22 10:45:28 -0500

    Exercise 2 complete

--- scripts/02-get-form-vals.js
+++ scripts/02-get-form-vals.js
@@ -1,1 +1,8 @@
-"use strict";
+'use strict'
+
+function getFormValue() {
+    event.preventDefault()
+    let form = document.getElementById('form1')
+    console.log('First name: ' + form.fname.value)
+    console.log('Last name: ' + form.lname.value)
+}
