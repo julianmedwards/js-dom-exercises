0bfb6d68b3b9bcba2be8b50905b1f6e5b846f11e
julianmedwards <julian.michael.edwards@gmail.com>
2022-06-22 11:00:26 -0500

    Exercise 4 complete.

--- scripts/04-get-vals-on-submit.js
+++ scripts/04-get-vals-on-submit.js
@@ -1,1 +1,15 @@
-"use strict";
+'use strict'
+
+function getAttributes() {
+    let hyperLink = document.getElementById('w3r')
+    let attrs = []
+    attrs.push(hyperLink.getAttribute('href'))
+    attrs.push(hyperLink.getAttribute('hreflang'))
+    attrs.push(hyperLink.getAttribute('rel'))
+    attrs.push(hyperLink.getAttribute('target'))
+    attrs.push(hyperLink.getAttribute('type'))
+
+    for (let attr of attrs) {
+        console.log(attr)
+    }
+}
